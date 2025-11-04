04a09204bf31f965919d59ede37fca8b1cf1ca19
duanluyao1 <duanluyao1@jd.com>
2025-11-04 11:01:52 +0800

    秀发i目录

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,9 +1,3 @@
 import { navbar } from "vuepress-theme-hope";
 
-export default navbar([
-  "/",
-  "/Java/",
-  "/其他/",
-  "/面试/",
-  "/随笔/",
-]);
+export default navbar(["/","/随笔/", "/Java/", "/其他/", "/面试/"]);
